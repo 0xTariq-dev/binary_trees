ea8b87648c89ab2d0800b474fe17c384215d9556
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-11-27 19:34:37 +0200

    feat: Add insertion and deletion functions

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+* *binary_tree_insert_right - Inserts a node as the right child of another node.
+* 
+* @parent: A pointer to the node to insert in the right-child.
+* @value: The value to store in the right node.
+* Return: A pointer to the created node
+*       or NULL on failure or if parent is NULL.
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+    if (parent == NULL)
+        return NULL;
+
+    new = binary_tree_node(parent, value);
+    if (!new)
+        return NULL;
+
+    if (parent->right)
+    {
+        new->right = parent->right;
+        new->right->parent = new;
+    }
+    parent->right = new;
+
+    return new;
+}
