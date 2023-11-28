35afa34c4543f65a6ca00e03d84ab4a5a2b6bf9d
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-11-28 03:05:44 +0200

    feat: Add post-order function (task8) with some style modification to the header file binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -21,16 +21,35 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+/* `binary_tree_print.c` NOT a requirement */
 void binary_tree_print(const binary_tree_t *);
+
+/* `0-binary_tree_node.c` */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* `1-binary_tree_insert_left.c` */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
+
+/* `2-binary_tree_insert_right.c` */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* `3-binary_tree_delete.c` */
 void binary_tree_delete(binary_tree_t *tree);
+
+/* `4-binary_tree_is_leaf.c` */
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* `5-binary_tree_is_root.c` */
 int binary_tree_is_root(const binary_tree_t *node);
+
+/* `6-binary_tree_preorder.c` */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* `7-binary_tree_inorder.c` */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void print_num(int n);
+
+/* `8-binary_tree_postorder.c` */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
 
 #endif /* _BINARY_TREES_H_ */
